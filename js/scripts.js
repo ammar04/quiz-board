79ec6fafeb70cb58480979c3ff4b32c5598efaad
Your Name <you@example.com>
2018-08-26 22:18:42 +0300

    added user interface logic to js

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,17 @@ if(answer2 == "HTML") {
 if(answer3 == "Yes") {
  correct+ = 10;
 }
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+  $(#submit).click(fuction(event) {
+    event.preventdefault();
+    var answer1= document.quiz.answer1.val();
+    var answer2= document.quiz.answer2.val();
+    var answer3= document.quiz.answer3.val();
+    var correct=0;
+    $("#quiz").hide();
+    $("#score").show()
+  });
+});
